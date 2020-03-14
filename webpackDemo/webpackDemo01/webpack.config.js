82c812a73d174787bfcf0a8d669139ea44f9967e
penghantian <penghantian@namibox.com>
2020-03-14 20:22:30 +0800

    css文件移动下目录

--- webpackDemo/webpackDemo01/webpack.config.js
+++ webpackDemo/webpackDemo01/webpack.config.js
@@ -3,17 +3,30 @@ const webpack = require('webpack');
 // 1. 自动在内存中根据指定页面生成一个内存中的页面
 // 2.自动把打包好的bundle.js追加到内存的页面中
 const  htmlWebpackPlugin = require('html-webpack-plugin');
-// 这个配置文件，其实就是一个js文件，通过Node中的模块操作，向外配置了一个配置对象
+/*
+    这个配置文件，其实就是一个js文件，通过Node中的模块操作，向外配置了一个配置对象
+    module.exports语法是COMOMONJS语法
+*/
 module.exports = {
     // 入口文件：表示要webpack打包哪个文件
     entry: path.join(__dirname, './src/main.js'),
-    //出口：
+    // 多个入口,多页面应用,单页面应用架构和业务代码分离
+    // entry: [path.join(__dirname, './src/main.js'), path.join(__dirname, './src/main2.js')],
+    // 第三种写法,比较推荐
+    // entry: {
+    //     index: path.join(__dirname, './src/main.js'),
+    //     index2: [path.join(__dirname, './src/main2.js'), path.join(__dirname, './src/main3.js')]
+    // }
+    
+    //出口: 一个或则多个,自定义规则,配置CDN
     output: {
         // 指定打包好的文件，输出到哪个目录中去
         path: path.join(__dirname, './dist'),
         // 指定输出文件的名称
         filename: 'bundle.js'
+        // filename: '[name].min.[hash:5].js'  // 多个入口对应多个出口的配置方法
     },
+
     // 
     devServer: {
         open: true,  // 自动打开浏览器
@@ -21,6 +34,19 @@ module.exports = {
         contentBase: 'src',  //指定托管的根目录
         hot: true,
     },
+
+    /*
+        参与打包的整个过程,具体有打包的优化和压缩,配置编译时的变量,极其灵活
+        常用的plugins:
+            优化相关:
+                CommonChunkPlugin
+                UglifyjsWebpackPlugin
+            功能相关:
+                ExtractTextWebpackPlugin: css提取出来打包单独文件
+                HtmlWebpackPlugin: 生成html
+                HotModuleReplacementPlugin: 模块热更新
+                CopyWebpackPlugin: 第三方文件打包
+    */
     plugins: [
         new webpack.HotModuleReplacementPlugin(),  // 
 
@@ -30,6 +56,22 @@ module.exports = {
             filename: 'index.html',
         }),
     ],
+
+    /*
+        loaders: 处理文件,转化为模块
+        常用loader:
+            编译相关:
+                babel-loader
+                ts-loader
+            样式相关:
+                style-loader
+                css-loader
+                less-loader
+                postcss-loader
+            文件相关:
+                file-loader
+                url-loader
+    */
     module: {  // 这个节点，用于配置所有的第三方加载器
         rules: [
             {
