e158c2ab76af416f022ea4221227384334118a47
conk <18170872531@163.com>
2020-07-14 23:49:11 +0800

    删除有序数组中的重复项目以及旋转数组

--- 11_算法/06_数组/01_list03.js
+++ 11_算法/06_数组/01_list03.js
@@ -1,25 +1,6 @@
 // 旋转数组: 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
 
 /*
-    自己的方法:
-        关键点: 
-            1. 循环k次
-            2. 在左边添加元素,值为最右边那个值
-            3. 删除左右的那个值
-*/
-var rotate = function(nums, k) {
-    if(k <= 0){
-        return nums;
-       }
-    k = k % nums.length;  // 优化
-    for(var i=0; i<k; i++){
-            nums.splice(0, 0, nums[nums.length -1]);
-            nums.splice(nums.length-1, 1);
-    }
-    return nums;
-};
-
-/*
     暴力破解法: 时间复杂度O(n*k),空间复杂度O(1)
         关键点:
             1. 每次将数组旋转1个元素
@@ -37,57 +18,6 @@ var rotate2 = function(nums, k){
             init = temp;
         }
     }
-}
-
-/*
-    使用额外的数组: 时间复杂度O(n), 空间复杂度O(n)
-        关键点:
-            1. 我们可以用一个额外的数组来将每个元素放到正确的位置上，
-            2. 原本数组里下标为i的我们把它放到 (i+k)%数组长度的位置。
-            3. 然后把新的数组拷贝到原数组中。
-*/
-var rotate3 = function(nums, k) {
-    if(k <= 0){
-        return nums;
-    }
-    var newNums = new Array(nums.length);
-    console.log('---',newNums);
-    for(var i=0; i<nums.length; i++){   
-        // newNums.splice((i+k)%nums.length, 1, nums[i]);
-        newNums[(i+k)%nums.length] = nums[i];
-    }
-    return newNums;
-
-}
-
-/*
-    使用环状替换: 时间复杂度O(n), 空间复杂度O(1)
-        关键点:
-            [1,2,3,4,5,6,7], 2
-            1--->3  换座位(0号位置空出来)----> (0 + 2) % 7 跳到2号的位置
-            3--->5  换座位(2号位置被挤出来)---> (2 + 2) % 7 跳到4号的位置
-            5--->7  换座位(4号位置被挤出来)---> (4 + 2) % 7 跳到6号的位置
-            7--->2  换座位(6号位置被挤出来)---> (6 + 2) % 7 跳到1号的位置
-            2--->4  换座位(1号位置被挤出来)---> (1 + 2) % 7 跳到3号的位置
-            4--->6  换座位(3号位置被挤出来)---> (3 + 2) % 7 跳到5号的位置
-            6--->1  换座位(5号位置被挤出来)---> (5 + 2) % 7 跳到0号的位置, 0号位置没有人
-*/
-var rotate4 = function(nums, k) {
-    var count = 0;  // n个同学,需要换n次位置
-    var k = k % nums.length;
-    for(var i=0; count<nums.length; i++) {
-        var current = i;  // 空位置
-        var prev = nums[current];
-        do{
-            var next = (k + current) % nums.length;
-            var temp = nums[next];
-            nums[next] = prev;
-            prev = temp;
-            current = next;
-            count++;
-        }while(current != i);
-    }
-    return nums;
 }
 
 /*
