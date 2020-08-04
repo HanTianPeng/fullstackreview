bd6891bf37f8e77d266509263aefa1653d26ff3e
conk <18170872531@163.com>
2020-08-05 01:01:29 +0800

    二叉树的层序遍历，层次遍历

--- 11_算法/04_二叉树/02_二叉树遍历.js
+++ 11_算法/04_二叉树/02_二叉树遍历.js
@@ -1,6 +1,5 @@
-// 二叉树的前序遍历: A -> B -> D -> E -> C -> F
+// 二叉树的前序遍历: A -> B -> D -> E -> C -> F ： 递归方法
 var preorderTraversal = function(root) {
-    // 二叉树前序遍历: 递归方法
     let result = [];
     // 递归函数
     let preorderTraversalNode = (node) => {
@@ -18,9 +17,8 @@ var preorderTraversal = function(root) {
     return result;
 };
 
-// 二叉树的前序遍历: A -> B -> D -> E -> C -> F
+// 二叉树的前序遍历: A -> B -> D -> E -> C -> F ： 迭代方法(栈)
 var preorderTraversal = function(root) {
-    // 二叉树前序遍历: 迭代方法
     let result = [],
         stack = [];
     // 将根节点入栈
@@ -39,9 +37,8 @@ var preorderTraversal = function(root) {
     return result;
 };
 
-// 二叉树的后序遍历: D -> E -> B -> F -> C -> A
+// 二叉树的后序遍历: D -> E -> B -> F -> C -> A : 递归
 var postorderTraversal = function(root) {
-    // 二叉树的后序遍历: 递归
     let result = [];
     // 定义递归函数
     let postorderTraversalNode = (node) => {
@@ -59,9 +56,8 @@ var postorderTraversal = function(root) {
     return result;
 };
 
-// 二叉树的后序遍历: D -> E -> B -> F -> C -> A
+// 二叉树的后序遍历: D -> E -> B -> F -> C -> A : 迭代方法(栈)
 var postorderTraversal = function(root) {
-    // 二叉树的后序遍历: 迭代方法
     let result = [],
         stack = [];
     // 将根节点入栈
@@ -80,9 +76,8 @@ var postorderTraversal = function(root) {
     return result;
 };
 
-// 二叉树的中序遍历: D -> B -> E -> A -> C -> F
+// 二叉树的中序遍历: D -> B -> E -> A -> C -> F : 递归
 var inorderTraversal = function(root) {
-    // 二叉树的中序遍历: 递归
     let result = [];
     // 定义递归函数
     let inorderTraversalNode = (node) => {
@@ -98,9 +93,8 @@ var inorderTraversal = function(root) {
     return result;
 }
 
-// 二叉树的中序遍历: D -> B -> E -> A -> C -> F
+// 二叉树的中序遍历: D -> B -> E -> A -> C -> F : 迭代方法(栈)
 var inorderTraversal = function(root) {
-    // 二叉树的中序遍历: 迭代方法
     let result = [],
         stack = [],
         curNode = root;
@@ -118,4 +112,113 @@ var inorderTraversal = function(root) {
         // 如果当前节点没有right值,则下一轮为其父节点;如果当前节点有right值,则下一轮为右子树节点
         curNode = curNode.right;
     }
+};
+
+// 二叉树的层序遍历: 从左至右，一层一层遍历: 深度优先搜索: DFS
+var leverOrder = function(root) {
+    let result = [];
+    // 边界处理
+    if(!root) return result;
+    // 深度优先搜索函数
+    let DFS = (node, step) => {
+        if(node) {
+            // 每一层没有数组存数据,就先创建一个空数组
+            if(!node[step]) node[step] = [];
+            // 写入值
+            node[step].push(node.val);
+            // 左子树深度优先
+            DFS(node.left, step+1);
+            // 右子树深度优先
+            DFS(node.right, step+1);
+        }
+    };
+    // 开始深度优先搜索
+    DFS(root, 0);
+    // 返回结果
+    return result;
+};
+
+// 二叉树的层序遍历: 从左至右, 一层一层遍历: 广度优先搜索: BFS(采用双端队列)
+var leverOrder= function(node) {
+    let result = [],
+        deque = [];
+    // 边界判断
+    if(!root) return result;
+    // 将根节点入队列
+    deque.push(root);
+    // 遍历
+    while(deque.length) {
+        // 获取队列长度
+        let len = deque.length;
+        // 定义存储当前层的数组
+        let leverResult = [];
+        // 遍历
+        while(len--) {
+            // 获取当前节点
+            let curNode = deque.shift();
+            // 写入值
+            leverResult.push(curNode.val);
+            // 如果当前节点有左子树，压入队列
+            if(curNode.left) deque.push(curNode.left);
+            // 如果当前节点有右子树,再压入队列
+            if(curNode.right) deque.push(curNode.right);
+        }
+        // 将一层值写入
+        result.push(leverResult);
+    } 
+    return result;
+};
+
+// 二叉树的层次遍历: 从底向上,从左至右,一层一层遍历: 广度优先搜索: DFS
+var levelOrderBottom = function(root) {
+    let result = [];
+    // 边界处理
+    if(!root) return result;
+    // DFS函数
+    let DFS = (node, step) => {
+        if(node) {
+            // 每一层没有数组存数据,就先创建一个空数组
+            if(!result[step]) result[step] = [];
+            // 写入值
+            result[step].push(node.val);
+            // 左子树深度优先遍历
+            DFS(node.left, step+1);
+            // 右子树深度优先遍历
+            DFS(node.right, step+1);
+        }
+    };
+    // 从根节点深度优先
+    DFS(root, 0);
+    return result.reverse();
+};
+
+// 二叉树的层次遍历: 从底向上,从左至右,一层一层遍历: 广度优先搜索: BFS(双端队列)
+var levelOrderBottom = function(root) {
+    let result = [],
+        deque = [];
+    // 边界判断
+    if(!root) return result;
+    // 将根节点入队列
+    deque.push(root);
+    // 遍历
+    while(deque.length) {
+        // 获取队列长度
+        let len = deque.length;
+        // 定义存储当前层的数组
+        let levelResult = [];
+        // 遍历
+        while(len--) {
+            // 获取当前节点
+            let curNode = deque.shift();
+            // 写入值
+            levelResult.push(curNode.val);
+            // 如果当前节点有左子树,压入队列
+            if(curNode.left) deque.push(curNode.left); 
+            // 如果当前节点有右子树,压入队列
+            if(curNode.right) deque.push(curNode.right);
+        }
+        // 将第一层值插入进入
+        result.unshift(levelResult);
+    } 
+    return result;
 };
