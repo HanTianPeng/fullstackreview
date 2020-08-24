f065bf88ed1d4e331997c78afa3728b1d3a8e116
conk <18170872531@163.com>
2020-08-24 23:33:35 +0800

    希尔排序，插入排序，快排三个排序算法的总结与思考

--- 11_算法/013_sort.js
+++ 11_算法/013_sort.js
@@ -7,23 +7,21 @@ var swap = (arr, i, j) => {
 // 快排一次函数
 var partition = (arr, left, right) => {
     // 取中间项为基准
-    let datum = arr[Math.floor(Math.random()*(right - left + 1)) + left],
-        i = left,
-        j = right;
+    let datum = arr[Math.floor(Math.random()*(right - left + 1)) + left];
     // 开始调整
-    while(i < j) {
+    while(left <= right) {
         // 左指针右移
-        while(arr[i] < datum) i++;
+        while(arr[left] < datum) left++;
         // 右指针左移
-        while(arr[j] > datum) j--;
+        while(arr[right] > datum) right--;
         // 交换
-        if(i < j) {
-            swap(arr, i, j);
-            i++;
-            j--;
+        if(left <= right) {
+            swap(arr, left, right);
+            left++;
+            right--;
         }
     }
-    return i;
+    return left;
 };
 // 快排递归函数
 var quick = (arr, left, right) => {
@@ -37,11 +35,52 @@ var quick = (arr, left, right) => {
         if(index < right) quick(arr, index, right);
     }
 };
-// 快排: 分治策略 + 从小到大排序 + 时间复杂度O(nlogn) + 空间复杂度O(nlogn)
+// 快排: 分治策略 + 从小到大排序 + 时间复杂度O(nlogn) + 空间复杂度O(nlogn) + 不稳定
 var quickSort = (arr) => {
     quick(arr, 0, arr.length - 1);
 }
-// 测试
-let arr = [1, 3, 2, 5, 4]
-quickSort(arr)
-console.log(arr) // [1, 2, 3, 4, 5]
+
+// 插入排序: 遍历 + 时间复杂度O(n^2) + 空间复杂度O(1) + 稳定
+var insertionSort = (arr) => {
+    // 边界处理
+    if(arr.length <= 1) return arr;
+    // 遍历
+    for(let i=1; i<arr.length; i++) {
+        // 对于未排序数据,在已排序序列中从后向前扫描,找到相应位置并插入
+        let preIndex = i - 1,
+            current = arr[i];
+        // 从后往前倒序依次比较
+        while(preIndex >= 0 && current < arr[preIndex]) {
+            // 依次往后挪动
+            arr[preIndex + 1] = arr[preIndex];
+            preIndex--;
+        }
+        // 找到当前元素适合的位置
+        arr[preIndex + 1] = current;
+    }
+    return arr;
+};
+
+// 希尔排序: 步长 + 时间复杂度O(nlogn),同时和步长有关系 + 空间复杂度O(1) + 不稳定
+var shellSort = (arr) => {
+    // 按照步长进行分组
+    for(let gap=Math.floor(arr.length/2); gap>0; gap=Math.floor(gap/2)) {
+        // 插入排序
+        for(let i=gap; i<arr.length; i++) {
+            let preIndex = i - gap,
+                current = arr[i];
+            // 从后往前倒序依次比较
+            while(preIndex >= 0 && current < arr[preIndex]) {
+                // 依次往后移动
+                arr[preIndex + gap] = arr[preIndex];
+                preIndex -= gap;
+            }
+            // 找到当前元素适合的位置
+            arr[preIndex + gap] = current;
+        }
+    }
+    return arr;
+};
+let arr = [59, 20, 17, 36, 98, 14, 23, 83, 13, 28];
+quickSort(arr);
+console.log(arr);
